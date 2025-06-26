0c8cbff9c528471ff5fd4e12d198f43a0d0faa51
Ishiva Oliver <ishivaoliver@gmail.com>
2025-06-26 22:30:43 +0300

    search bar changes to smart search bar

--- frontend/src/app/components/SmartSearch.tsx
+++ frontend/src/app/components/SmartSearch.tsx
@@ -2,13 +2,13 @@
 
 import { Search } from "lucide-react";
 
-export default function SearchBar() {
+export default function SmartSearch() {
   return (
-    <div className="relative w-full max-w-xl">
+    <div className="relative w-full max-w-2xl mt-6">
       <input
         type="text"
-        placeholder="Find your dream preferences"
-        className="w-full py-4 px-6 pr-14 rounded-full shadow-xl text-lg placeholder-gray-500 bg-white focus:outline-none focus:ring-2 focus:ring-black"
+        placeholder="Where do you dream of living?"
+        className="w-full py-4 px-6 pr-14 rounded-full shadow-lg text-lg placeholder-gray-500 bg-white focus:outline-none focus:ring-2 focus:ring-primary"
       />
       <button
         className="absolute right-3 top-1/2 transform -translate-y-1/2 bg-black text-white p-2 rounded-full hover:bg-gray-800 transition"
